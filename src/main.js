54d9c0c466676dee6679bc2b88919c2f4a9fa5f4
ckx <958436099@qq.com>
2020-02-26 16:10:29 +0800

    新增资源提交本地

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import elementui from 'element-ui'// 引入elementui
 import 'element-ui/lib/theme-chalk/index.css'// 引入样式文件
+import './styles/index.less' // 引入样式
 
 Vue.config.productionTip = false
 Vue.use(elementui) // 将element注册到vue实例上便可以全局使用
