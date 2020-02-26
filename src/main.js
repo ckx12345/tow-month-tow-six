0128367c3923cf450788936e7cdd2286468ca802
ckx <958436099@qq.com>
2020-02-26 15:49:04 +0800

    安装elementui

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import elementui from 'element-ui'// 引入elementui
+import 'element-ui/lib/theme-chalk/index.css'// 引入样式文件
 
 Vue.config.productionTip = false
-
+Vue.use(elementui) // 将element注册到vue实例上便可以全局使用
 new Vue({
   router,
   store,
